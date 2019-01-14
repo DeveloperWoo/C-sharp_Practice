0bb8c10cd8f68a67901b0f0ff73d2c15137f1662
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-01-14 17:25:52 -0500

    Add files via upload
    Variable, DataType

---
+++ CSharp with Mosh/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 20.95f;
+            //char character = 'A';
+            //string firstName = "Hyunju";
+            //bool isWorking = false;
+
+            var number = 2; //By default, 2 is considered as an integer
+            var count = 10;
+            var totalPrice = 20.95f; //By default, a real number is treated as double
+            var character = 'A';
+            var firstName = "Hyunju";
+            var isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+          
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue); // 0 255
+        }
+    }
+}
