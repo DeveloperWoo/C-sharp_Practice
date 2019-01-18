b70c427c5dd524542ce7450aa063f85f55e23462
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-01-18 16:18:46 -0500

    Add files via upload

---
+++ CSharp with Mosh/AccessModifier/AccessModifier/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AccessModifier
+{
+    public class Person
+    {
+        private DateTime _birthday;
+
+        public void SetBirthday(DateTime birthday)
+        {
+            this._birthday = birthday;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthday;
+        }
+    }
+}
---
+++ CSharp with Mosh/AccessModifier/AccessModifier/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AccessModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            /*
+             * using setters and getters, 
+             * we know what it does but cannot know how it works inside the method. 
+             * This is called information hiding
+            */
+            person.SetBirthday(new DateTime(2019, 1, 18)); 
+            Console.WriteLine(person.GetBirthdate()); //2019-01-18 12:00:00 AM
+        }
+    }
+}
